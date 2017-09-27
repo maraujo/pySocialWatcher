76a8770105f673b48e51c3b5d35908c8ffe73c69
Matheus Araújo <matheus>
2017-09-27 13:22:09 -0500

    Adding zipcodes to ny data collection AND fixing others relationship

--- pysocialwatcher/__init__.py
+++ pysocialwatcher/__init__.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     watcherAPI.load_credentials_file("credentials.csv")
     # watcherAPI.check_tokens_account_valid()
-    watcherAPI.run_data_collection("./input_examples/India_test_small_not_scholarities.json")
+    watcherAPI.run_data_collection("./input_examples/proof_of_concept.json")
 
