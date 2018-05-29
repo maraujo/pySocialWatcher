0f2b374d80b466af8d57bb5ecc210337aebe3008
Matheus <arauj021@umn.edu>
2018-05-29 11:22:39 +0100

    Version/README updade

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pysocialwatcher',
-      version='0.1g',
+      version='0.1h',
       description='pySocialWatcher : Social Watcher through Facebook Marketing API',
       url='https://github.com/maraujo/pySocialWatcher',
       long_description="""
